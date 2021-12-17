be08e23cbfdb79543dfaea1fee8009be5c0c304b
PlanetesEmp <38735769+PlanetesEmp@users.noreply.github.com>
2021-12-16 19:17:26 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,25 +1,19 @@
 
-
-
-
-
-//function makeList(event){
-//	console.log(document.getElementById("movie_title").value);
-//}
-var movietitle=[];
+var gametitle=[];
 var searchtitle=[];
 var str;
 window.ul = document.getElementById("searchlist").getElementsByTagName("li");
 window.title;
 
+
 function searchbox() {
     var input, filter, ul, li, a, i;
     searchtitle=[];
     input = document.getElementById('myInput');
-        for(i=0;i<movietitle.length;i++){
-            if (movietitle[i].startsWith(input.value)) {
-                searchtitle.push(movietitle[i]);}
-        }
+    for(i=0;i<gametitle.length;i++){
+        if (gametitle[i].startsWith(input.value)) {
+            searchtitle.push(gametitle[i]);}
+    }
     ul = document.getElementById("searchlist").getElementsByTagName("li");
     for(i in ul) {
         if(searchtitle[i]==null){
@@ -34,50 +28,44 @@ function searchbox() {
         d[i].addEventListener('click',function() {
             document.getElementById("myInput").value = this.textContent;
         });
-}}
-    d3.csv("Game_K-mean.csv").then(function (data) {
-        /*var movietree= new mttree();
-        data.forEach(function (d) {
-            movietree.insert(d.title);
-        })*/
+    }}
+d3.csv("Game_K-mean.csv").then(function (data) {
 
-        searchtitle = document.getElementById('myInput');
-        data.forEach(function (d) {
-            movietitle.push(d.title);
-        })
-        console.log(movietitle);
 
-        /*
-        movietree.searchtitle("Nobaaddy");
-        console.log(forli);*/
+    searchtitle = document.getElementById('myInput');
+    data.forEach(function (d) {
+        gametitle.push(d.Name);
+    })
+    console.log(gametitle);
 
 
 
-        window.makeList = function(){
-	    d3.select("#movie_list").selectAll("li").remove();
-            title = d3.select("#myInput").property("value");
-            console.log(title);
 
-            var cluster = [];
-            var titles = [];
+    window.makeList = function(){
+        d3.select("#game_list").selectAll("li").remove();
+        title = d3.select("#myInput").property("value");
+        console.log(title);
 
-
-            data.forEach(function(d){
-                if (title == d.title){
-                    cluster.push(d.cluster);
-                }
-
-            });
-
-            var cluster_data = data.filter((d) => cluster.includes(d.cluster));
+        var cluster = [];
+        var titles = [];
 
 
+        data.forEach(function(d){
+            if (title == d.Name){
+                cluster.push(d.cluster);
+            }
 
-            d3.select("#movie_list")
-              .selectAll("li")
-              .data(cluster_data)
-              .enter()
-              .append("li")
-              .text((d) => d.title);
-        }
-    });
+        });
+
+        var cluster_data = data.filter((d) => cluster.includes(d.cluster));
+
+
+
+        d3.select("#game_list")
+            .selectAll("li")
+            .data(cluster_data)
+            .enter()
+            .append("li")
+            .text((d) => d.Name);
+    }
+});
